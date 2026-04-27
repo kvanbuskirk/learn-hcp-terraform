bbc9058728aadc88560602e7ab5bb1adc4ce2f96
kvanbuskirk <167045162+kvanbuskirk@users.noreply.github.com>
2026-04-26 23:50:24 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,4 +5,7 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.amazon-linux-2.arn
 }
-
+output "instance_ami" {
+   description = "AMI used to create the EC2 instance."
+   value       = aws_instance.app_server.ami
+}
