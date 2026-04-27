0475d77820fb491a8379f3de4a6309a8bdfeebf3
kvanbuskirk <167045162+kvanbuskirk@users.noreply.github.com>
2026-04-27 00:01:43 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "instance_ami" {
-  value = aws_instance.amazon-linux-2.ami
-}
+#output "instance_ami" {
+#  value = aws_instance.amazon-linux-2.ami
+#}
 
 output "instance_arn" {
   value = aws_instance.amazon-linux-2.arn
