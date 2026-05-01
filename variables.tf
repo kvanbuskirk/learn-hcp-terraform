0c08bf22324aee8831a19fb82e0353a0be958f53
kvanbuskirk <167045162+kvanbuskirk@users.noreply.github.com>
2026-04-30 22:18:31 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,14 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "prefix" {
+  descriptioin = "this is the environment your webapp will be prefixed with. dev, qa, or prod"
+}
+
+variable "name" {
+  description = "Your name to attach to the webapp address"
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
